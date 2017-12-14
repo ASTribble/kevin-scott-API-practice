556273b8049154efa9ab0120bbac9473296642cc
A. Scott Tribble <a.scott.tribble@gmail.com>
2017-12-13 16:06:20 -0800

    newIndex.js file

---
+++ newIndex.js
@@ -0,0 +1,16 @@
+'use strict';
+/* global */
+
+// App Requirements
+
+// Accept a user search term
+function getUserSearchTerm(){
+  $('form').submit(function(event){
+    event.preventDefault();
+    const 
+  });
+}
+
+// Get JSON from the YouTube API based on the user search term
+
+// Display the thumbnail image of the returned videos
